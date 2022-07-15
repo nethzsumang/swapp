a3a092ac96a0dfe4df682e3b939643eb3f91b1ad
Kenneth Sumang <kennethsumang08@gmail.com>
2022-07-15 18:59:43 +0800

    feat: on click handler for list

--- src/controller/Main.controller.ts
+++ src/controller/Main.controller.ts
@@ -1,6 +1,8 @@
 import MessageBox from "sap/m/MessageBox";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import Event from "sap/ui/base/Event";
 import BaseController from "./BaseController";
+import BindingMode from "sap/ui/model/BindingMode";
 
 /**
  * @namespace com.kennethsumang.swapp.controller
@@ -10,14 +12,12 @@ export default class Main extends BaseController {
 	onInit() {
 		// swapiModel data model
 		let swapiModel = new JSONModel();
-		swapiModel.loadData('https://swapi.dev/api/people', {'limit': 100}, false, 'GET', false, true, {});
+		swapiModel.loadData('https://swapi.dev/api/people', {}, true, 'GET', false, true, {});
 		this.getView().setModel(swapiModel, 'swapi');
-		console.log(this.getView().getModel('swapi'));
 
 		// state data model
-		let stateModel = new JSONModel({
-			'selectedId': null
-		});
+		let stateModel = new JSONModel({});
+		stateModel.setDefaultBindingMode(BindingMode.OneWay);
 		this.getView().setModel(stateModel, 'state');
 	}
 	
@@ -25,9 +25,17 @@ export default class Main extends BaseController {
 		MessageBox.show("Hello World!");
 	}
 
-	public handleListItemClick(event : Event) : void {
-		console.log(event);
-		console.log(event.target);
+	public handleListItemClick(event : Event, name : string) : void {
+		const peopleList = this.getView().getModel('swapi').getProperty('/results');
+		let selectedPeople = {};
+		peopleList.forEach((people : People) => {
+			if (people.name === name) {
+				selectedPeople = people;
+			}
+		});
+		let newState = new JSONModel(selectedPeople);
+		newState.setDefaultBindingMode(BindingMode.OneWay);
+		this.getView().setModel(newState, 'state');
 	}
 
 }
---
+++ src/interfaces/People.interface.ts
@@ -0,0 +1,21 @@
+/**
+ * Interface for People result in swapi.dev
+ */
+interface People {
+    name: string;
+    height: string|number;
+    mass: string|number;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: Array<string>;
+    species: Array<string>;
+    vehicles: Array<string>;
+    starships: Array<string>;
+    created: string;
+    edited: string;
+    url: string;
+}
